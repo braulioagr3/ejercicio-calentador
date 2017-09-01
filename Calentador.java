adf537a434b1da557f08798fb6439cddee8f8fa3
Braulio García <braulioagr3@gmail.com>
2017-08-31 22:31:18 -0500

    Se añadio la documentacion con JavaDoc

--- Calentador.java
+++ Calentador.java
@@ -5,14 +5,18 @@ public class Calentador
     private int incremento;
     private int min;
     private int max;
-    public Calentador(int maxI, int minI)
+    public Calentador(int maxI, int minI, int incremento)
     {
         // initialise instance variables
         min=minI;
         max=maxI;
         temperatura = 15;
-        incremento = 3;
+        this.incremento = incremento;
     }
+    /**
+     * Aumenta la temperatura con respecto al incremento solicitado por el
+     * usuario
+     */
     public void calentar()
     {
         int cambio;
@@ -22,6 +26,10 @@ public class Calentador
             temperatura = cambio;
         }
     }
+    /**
+     * Reduce la temperatura con respecto al incremento solicitado por el
+     * usuario
+     */
     public void enfriar()
     {
         int cambio;
@@ -31,10 +39,19 @@ public class Calentador
             temperatura = cambio;
         }
     }
+    /**
+     * Muestra la temperatura actual
+     * @return temepratura regresa el valor de la temperatura actual
+     */
     public int termostato()
     {
         return temperatura;
     }
+    /**
+     * Modifica el valor del incremento siempre y cuando no reciba un numero
+     * negativo
+     * @Param recibe el nuevo vlaor de aumento que quiere el usuario
+     */
     public void aumentaIncremento(int aumento)
     {
         if(aumento>0)
